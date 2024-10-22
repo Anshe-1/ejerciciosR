d42dc4e98646ffd3e21907f861317b1c6250bf60
Anshe-1 <anyelosalas533@gmail.com>
2024-10-22 17:57:21 -0500

    Update 6.R

--- 6.R
+++ 6.R
@@ -3,15 +3,15 @@ numero_secreto <- sample(1:100, 1)
 intentos <- 0
 max_intentos <- 7
 
-cat("¡Bienvenido al juego de adivinanza!\n")
+cat(" Juego de adivinanza\n")
 cat("Tienes", max_intentos, "intentos para adivinar el número.\n")
 
 while (intentos < max_intentos) {
-  adivinanza <- as.integer(readline(prompt="Adivina el número (entre 1 y 100): "))
+  adivinanza <- as.integer(readline(prompt="Adivina el numero (entre 1 y 100): "))
   intentos <- intentos + 1
   
   if (adivinanza == numero_secreto) {
-    cat("¡Felicitaciones! Adivinaste el número en", intentos, "intentos.\n")
+    cat(" Adivinaste el número en", intentos, "intentos.\n")
     break
   } else if (adivinanza > numero_secreto) {
     cat("Es menor. Intenta de nuevo.\n")
@@ -19,7 +19,7 @@ while (intentos < max_intentos) {
     cat("Es mayor. Intenta de nuevo.\n")
   }
   if (intentos >= max_intentos) {
-    cat("¡Lo siento! Se te acabaron los intentos. El número era", numero_secreto, ".\n")
+    cat(" Se te acabaron los intentos. El número era", numero_secreto, ".\n")
   }
   
 }
